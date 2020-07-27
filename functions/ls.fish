0946d8b9a4400d187450926e18460f306708c341
Wojciech Siewierski <wojciech@siewierski.eu>
2020-07-27 03:02:28 +0200

    Fix indentation

--- functions/ls.fish
+++ functions/ls.fish
@@ -1,4 +1,4 @@
 function ls --description 'List contents of directory' --wraps=ls
-	set -l param --color=auto --group-directories-first
-        command ls $param $argv
+    set -l param --color=auto --group-directories-first
+    command ls $param $argv
 end
