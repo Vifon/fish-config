71bb277af8393668d2c0cc63aef2c31ba00c38d1
Wojciech Siewierski <wojciech@siewierski.eu>
2021-03-16 04:20:59 +0100

    Update the prompt against the upstream Fish 3.2.0

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,5 +1,6 @@
 function fish_prompt --description 'Write out the prompt'
     set -l last_pipestatus $pipestatus
+    set -lx __fish_last_status $status # Export for __fish_print_pipestatus.
     set -l normal (set_color normal)
 
     set -l fish_color_user green
@@ -8,9 +9,8 @@ function fish_prompt --description 'Write out the prompt'
 
     # Color the prompt differently when we're root
     set -l color_cwd $fish_color_cwd
-    set -l prefix
     set -l suffix '>'
-    if contains -- $USER root toor
+    if functions -q fish_is_root_user; and fish_is_root_user
         if set -q fish_color_cwd_root
             set color_cwd $fish_color_cwd_root
         end
@@ -41,7 +41,14 @@ function fish_prompt --description 'Write out the prompt'
     end
 
     # Write pipestatus
-    set -l prompt_status (__fish_print_pipestatus " [" "]" "|" (set_color $fish_color_status) (set_color --bold $fish_color_status) $last_pipestatus)
+    # If the status was carried over (e.g. after `set`), don't bold it.
+    set -l bold_flag --bold
+    set -q __fish_prompt_status_generation; or set -g __fish_prompt_status_generation $status_generation
+    if test $__fish_prompt_status_generation = $status_generation
+        set bold_flag
+    end
+    set __fish_prompt_status_generation $status_generation
+    set -l prompt_status (__fish_print_pipestatus "[" "]" "|" (set_color $fish_color_status) (set_color $bold_flag $fish_color_status) $last_pipestatus)
 
-    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal $vcs_status $normal $prompt_status $suffix " "
+    echo -n -s (set_color $fish_color_user) "$USER" $normal @ (set_color $color_host) (prompt_hostname) $normal ' ' (set_color $color_cwd) (prompt_pwd) $normal $vcs_status $normal " "$prompt_status $suffix " "
 end
